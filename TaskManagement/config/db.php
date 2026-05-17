99ad0db199129add9f05314a1a4b388a1ab56a29
Rifat Ahammed Arafat <rifatahammedarafat@gmail.com>
2026-05-17 19:41:04 +0600

    Added database connection code in db.php

--- TaskManagement/config/db.php
+++ TaskManagement/config/db.php
@@ -1,1 +1,12 @@
+<?php
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "task_management_db";
 
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+if (!$conn) {
+    die("Database Connection Failed: " . mysqli_connect_error());
+}
+?>
