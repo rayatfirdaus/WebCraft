322e6afba04c126ebd1c30fdcc81c373b5e67d59
Rifat Ahammed Arafat <rifatahammedarafat@gmail.com>
2026-05-17 20:19:33 +0600

    Add removeWorkspaceMember JavaScript function to handle asynchronous AJAX member deletion and dynamic DOM updates

--- TaskManagement/assets/js/workspace.js
+++ TaskManagement/assets/js/workspace.js
@@ -1,1 +1,22 @@
-
+function removeWorkspaceMember(memberRowId) {
+    if(confirm("Are you sure you want to remove this member?")) {
+        var xhttp = new XMLHttpRequest();
+        
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                if(this.responseText.trim() === "success") {
+                    var row = document.getElementById("member-row-" + memberRowId);
+                    if(row) {
+                        row.parentNode.removeChild(row);
+                    }
+                } else {
+                    alert("Failed to remove member!");
+                }
+            }
+        };
+        
+        xhttp.open("POST", "../controllers/WorkspaceController.php", true);
+        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhttp.send("action=remove_member&member_row_id=" + memberRowId);
+    }
+}
