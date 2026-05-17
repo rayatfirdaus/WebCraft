cda969d5f94f320dc329a69de6552f0d4a7990f8
Rifat Ahammed Arafat <rifatahammedarafat@gmail.com>
2026-05-17 20:03:24 +0600

    Add user authentication functions for registration, login, and email verification

--- TaskManagement/models/UserModel.php
+++ TaskManagement/models/UserModel.php
@@ -1,1 +1,27 @@
+<?php
+function registerUser($conn, $name, $email, $password) {
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+    
+    $sql = "INSERT INTO users (name, email, password_hash) VALUES ('$name', '$email', '$hashed_password')";
+    return mysqli_query($conn, $sql);
+}
 
+function loginUser($conn, $email, $password) {
+    $sql = "SELECT * FROM users WHERE email = '$email'";
+    $result = mysqli_query($conn, $sql);
+    
+    if (mysqli_num_rows($result) > 0) {
+        $user = mysqli_fetch_assoc($result);
+        if (password_verify($password, $user['password_hash'])) {
+            return $user;
+        }
+    }
+    return false;
+}
+
+function checkEmailExists($conn, $email) {
+    $sql = "SELECT id FROM users WHERE email = '$email'";
+    $result = mysqli_query($conn, $sql);
+    return mysqli_num_rows($result) > 0;
+}
+?>
